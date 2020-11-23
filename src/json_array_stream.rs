f3f8ca8baa8040c4384a33da670e7bbffe4f3278
TRACIP <contact@tracip.fr>
2020-11-23 10:12:35 +0100

    delete redundant stream wrappers, and improve error handling

--- src/json_array_stream.rs
+++ src/json_array_stream.rs
@@ -2,12 +2,20 @@ use futures::Stream;
 use pin_project::pin_project;
 use serde::Serialize;
 use std::{
-    fmt::Display,
+    fmt::{
+        Display,
+        Debug,
+    },
     pin::Pin,
     task::{
         Context,
         Poll,
     },
+};
+use actix_web::{
+    ResponseError,
+    http::StatusCode,
+    HttpResponse,
 };
 
 #[pin_project(project = JsonArrayStreamProject)]
@@ -33,13 +41,58 @@ where
     }
 }
 
+pub enum SerializedStreamError<E> {
+    SourceError(E),
+    SerializationError(serde_json::error::Error),
+}
+impl<E> Debug for SerializedStreamError<E>
+    where E: 'static + Sized + ResponseError
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            SerializedStreamError::SourceError(e) => Debug::fmt(e, f),
+            SerializedStreamError::SerializationError(e) => Debug::fmt(e, f),
+        }
+    }
+}
+impl<E> Display for SerializedStreamError<E>
+    where E: 'static + Sized + ResponseError
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            SerializedStreamError::SourceError(e) => Display::fmt(e, f),
+            SerializedStreamError::SerializationError(e) => Display::fmt(e, f),
+        }
+    }
+}
+impl<E> std::error::Error for SerializedStreamError<E>
+    where E: 'static + Sized + ResponseError {}
+
+impl<E> ResponseError for SerializedStreamError<E>
+    where E: 'static + Sized + ResponseError
+{
+    fn status_code(&self) -> StatusCode {
+        match self {
+            SerializedStreamError::SourceError(e) => e.status_code(),
+            SerializedStreamError::SerializationError(e) => e.status_code(),
+        }
+    }
+
+    fn error_response(&self) -> HttpResponse {
+        match self {
+            SerializedStreamError::SourceError(e) => e.error_response(),
+            SerializedStreamError::SerializationError(e) => e.error_response(),
+        }
+    }
+}
+
 impl<T, E, S> Stream for JsonArrayStream<S>
 where
     T: 'static + Serialize,
-    E: Display,
+    E: 'static + Sized + ResponseError,
     S: 'static + Sized + Stream<Item = Result<T, E>>,
 {
-    type Item = Result<String, String>;
+    type Item = Result<String, SerializedStreamError<E>>;
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         let JsonArrayStreamProject {
@@ -59,8 +112,8 @@ where
                 Poll::Ready(Some(res)) => {
                     let mut new_res = match res {
                         Ok(value) => serde_json::to_string(&value)
-                            .map_err(|msg| format!("Serialization error: {}", msg)),
-                        Err(source_error) => Err(format!("{}", source_error)),
+                            .map_err(|serialization_error| SerializedStreamError::SerializationError(serialization_error)),
+                        Err(source_error) => Err(SerializedStreamError::SourceError(source_error)),
                     };
                     new_res.iter_mut().for_each(|value_str| {
                         if *is_first {
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 #![feature(fn_traits)]
 #![feature(unboxed_closures)]
 
-pub mod actix_stream;
-pub mod inner_map_stream;
 pub mod json_array_stream;
+pub use json_array_stream::JsonArrayStream;
+
 pub mod owned_pool_stream;
+pub use owned_pool_stream::OwnedPoolStream;
