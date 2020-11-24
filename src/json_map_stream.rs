cdb1603758f76bc146f1d2a6051eb5997c460c7b
TRACIP <contact@tracip.fr>
2020-11-24 12:05:37 +0100

    compile ok

--- src/json_map_stream.rs
+++ src/json_map_stream.rs
@@ -14,6 +14,7 @@ use sqlx::{
     Postgres,
 };
 use std::{
+    marker::PhantomData,
     mem,
     pin::Pin,
     ptr::NonNull,
@@ -28,17 +29,21 @@ use crate::serialized_stream_error::SerializedStreamError;
 use State::*;
 
 #[pin_project(project = JsonMapStreamProject)]
-pub struct JsonMapStream<O, K, E, I, F, G> {
+pub struct JsonMapStream<O, E, I, F, G, A, B, K, T, W, WE> {
     #[pin]
     pool: PgPool,
     state: State<K>,
-    pinned_stream_opt: Option<Pin<Box<dyn 'static + Unpin + Stream<Item = Result<O, E>>>>>,
+    pinned_stream_opt: Option<Pin<Box<dyn 'static + Unpin + Stream<Item = Result<O, WE>>>>>,
     remaining_queries: I,
     error_transform: F,
     post_transform: G,
+    phantom: PhantomData<(E, A, B, T, W, WE)>,
 }
 
-impl<O, E, I, F, G, A, B, K, T, V, W> JsonMapStream<O, K, E, I, F, G>
+pub type JsonMapInnerStream<'a, T, F> =
+    futures::stream::MapErr<Pin<Box<dyn Send + Stream<Item = Result<T, sqlx::Error>>>>, &'a mut F>;
+
+impl<O, E, I, F, G, A, B, K, T, W, WE> JsonMapStream<O, E, I, F, G, A, B, K, T, W, WE>
 where
     O: 'static + Send + Unpin + Serialize,
     E: 'static + ResponseError,
@@ -46,31 +51,13 @@ where
     A: 'static + Send + IntoArguments<'static, Postgres>,
     B: 'static + Send + Sync + Fn(<Postgres as sqlx::Database>::Row) -> Result<T, sqlx::Error>,
     I: 'static + Iterator<Item = (K, sqlx::query::Map<'static, Postgres, B, A>)>,
-    K: 'static + Send + Unpin + Serialize,
+    K: 'static + Clone + Send + Unpin + Serialize,
     T: 'static + Send + Unpin,
-    V: 'static + Unpin + Stream<Item = Result<T, dyn ResponseError>>,
-    W: 'static + Unpin + Stream<Item = Result<O, dyn ResponseError>>,
-    G: 'static + Fn(V) -> W,
+    W: 'static + Unpin + Stream<Item = Result<O, WE>>,
+    WE: 'static + Sized + ResponseError,
+    G: 'static + Fn(JsonMapInnerStream<T, F>) -> W,
 {
-    pub fn from<Im, Gm, Am, Bm, Tm, Vm, Wm, Km>(
-        pool: PgPool,
-        queries: Im,
-        error_transform: F,
-        post_transform: Gm,
-    ) -> Pin<Box<JsonMapStream<O, Km, E, Im, F, Gm>>>
-    where
-        Am: 'static + Send + IntoArguments<'static, Postgres>,
-        Bm: 'static
-            + Send
-            + Sync
-            + Fn(<Postgres as sqlx::Database>::Row) -> Result<Tm, sqlx::Error>,
-        Im: 'static + Iterator<Item = (Km, sqlx::query::Map<'static, Postgres, Bm, Am>)>,
-        Km: 'static + Send + Unpin + Serialize,
-        Tm: 'static + Send + Unpin,
-        Vm: 'static + Unpin + Stream<Item = Result<Tm, dyn ResponseError>>,
-        Wm: 'static + Unpin + Stream<Item = Result<O, dyn ResponseError>>,
-        Gm: 'static + Fn(Vm) -> Wm,
-    {
+    pub fn from(pool: PgPool, queries: I, error_transform: F, post_transform: G) -> Pin<Box<Self>> {
         let unpinned = JsonMapStream {
             pool,
             state: Init,
@@ -78,6 +65,7 @@ where
             remaining_queries: queries,
             error_transform,
             post_transform,
+            phantom: PhantomData,
         };
         // I need to pin the struct here, to then get a pointer to pool which will
         // always be valid (because the struct can no longer be moved using the
@@ -93,6 +81,7 @@ where
         // TODO: load_next_query_and_update_state should be executed if current stream
         let pool_raw_ptr = &self.pool as *const PgPool;
         let JsonMapStreamProject {
+            phantom: _,
             pool: _,
             state,
             pinned_stream_opt,
@@ -122,47 +111,16 @@ where
             },
         }
     }
-}
 
-enum State<K> {
-    Init,
-    MustEmitGlobalOpeningAndItemOpening(K),
-    MustEmitItemClosingAndItemOpening(K),
-    MustEmitGlobalOpeningAndGlobalClosing,
-    MustEmitItemClosingAndGlobalClosing,
-    MustEmitStreamItem { is_first: bool },
-    Done,
-}
-
-fn indent(serialized_value: String, is_first: bool) -> String {
-    let lines: Vec<String> = serialized_value.split('\n').collect();
-    let mut text: String = lines.join("\n        ");
-    if is_first {
-        text.insert_str(0, "\n        ");
-    } else {
-        text.insert_str(0, ",\n        ");
-    }
-    text
-}
-
-impl<O, E, I, F, G, A, B, K, T, V, W> Stream for JsonMapStream<O, K, E, I, F, G>
-where
-    O: 'static + Send + Unpin + Serialize,
-    E: 'static + ResponseError,
-    F: 'static + Fn(sqlx::Error) -> E,
-    A: 'static + Send + IntoArguments<'static, Postgres>,
-    B: 'static + Send + Sync + Fn(<Postgres as sqlx::Database>::Row) -> Result<T, sqlx::Error>,
-    I: 'static + Iterator<Item = (K, sqlx::query::Map<'static, Postgres, B, A>)>,
-    K: 'static + Send + Unpin + Serialize,
-    T: 'static + Send + Unpin,
-    V: 'static + Unpin + Stream<Item = Result<T, dyn ResponseError>>,
-    W: 'static + Unpin + Stream<Item = Result<O, dyn ResponseError>>,
-    G: 'static + Fn(V) -> W,
-{
-    type Item = Result<Bytes, E>;
-
-    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+    fn new_state_poll_result(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+    ) -> (
+        State<K>,
+        Poll<Option<Result<Bytes, SerializedStreamError<WE>>>>,
+    ) {
         let JsonMapStreamProject {
+            phantom: _,
             pool: _,
             state,
             pinned_stream_opt,
@@ -174,68 +132,115 @@ where
             None => {
                 // Should not happen, because pinned_stream is never set to None after
                 // initialization
-                Poll::Pending
+                (state.clone(), Poll::Pending)
             },
             Some(pinned_stream) => {
                 let pinned_stream = pinned_stream.as_mut();
-                match *state {
-                    Init => {
+                match state {
+                    Init | MustLoad => {
                         // Should not happen, because state changes during initialization with
                         // the load_next_query_and_update_state call
-                        Poll::Pending
+                        (state.clone(), Poll::Pending)
                     },
-                    MustEmitGlobalOpeningAndItemOpening(key) => {
-                        *state = MustEmitStreamItem { is_first: true };
-                        Poll::Ready(Some(match serde_json::to_string(&key) {
+                    MustEmitGlobalOpeningAndItemOpening(key) => (
+                        MustEmitStreamItem { is_first: true },
+                        Poll::Ready(Some(match serde_json::to_string(key) {
                             Ok(serialized_key) => {
                                 let output = format!("{{\n    {}: [", serialized_key);
                                 Ok(Bytes::from(output))
                             },
                             Err(e) => Err(SerializedStreamError::SerializationError(e)),
-                        }))
-                    },
-                    MustEmitItemClosingAndItemOpening(key) => {
-                        *state = MustEmitStreamItem { is_first: true };
-                        Poll::Ready(Some(match serde_json::to_string(&key) {
+                        })),
+                    ),
+                    MustEmitItemClosingAndItemOpening(key) => (
+                        MustEmitStreamItem { is_first: true },
+                        Poll::Ready(Some(match serde_json::to_string(key) {
                             Ok(serialized_key) => {
                                 let output = format!("\n    ],\n    {}: [", serialized_key);
                                 Ok(Bytes::from(output))
                             },
                             Err(e) => Err(SerializedStreamError::SerializationError(e)),
-                        }))
-                    },
+                        })),
+                    ),
                     MustEmitGlobalOpeningAndGlobalClosing => {
-                        *state = Done;
-                        Poll::Ready(Some(Ok(Bytes::from(format!("{{}}")))))
+                        (Done, Poll::Ready(Some(Ok(Bytes::from(format!("{{}}"))))))
                     },
-                    MustEmitItemClosingAndGlobalClosing => {
-                        *state = Done;
-                        Poll::Ready(Some(Ok(Bytes::from(format!("\n    ]\n}}")))))
-                    },
+                    MustEmitItemClosingAndGlobalClosing => (
+                        Done,
+                        Poll::Ready(Some(Ok(Bytes::from(format!("\n    ]\n}}"))))),
+                    ),
                     MustEmitStreamItem { is_first } => match pinned_stream.poll_next(cx) {
-                        Poll::Pending => Poll::Pending,
-                        Poll::Ready(None) => {
-                            self.load_next_query_and_update_state();
-                            Poll::Pending
-                        },
-                        Poll::Ready(Some(Err(e))) => {
-                            Poll::Ready(Some(Err(SerializedStreamError::SourceError(e))))
-                        },
-                        Poll::Ready(Some(Ok(value))) => {
-                            if is_first {
-                                *state = MustEmitStreamItem { is_first: false };
-                            }
+                        Poll::Pending => (state.clone(), Poll::Pending),
+                        Poll::Ready(None) => (MustLoad, Poll::Pending),
+                        Poll::Ready(Some(Err(e))) => (
+                            state.clone(),
+                            Poll::Ready(Some(Err(SerializedStreamError::SourceError(e)))),
+                        ),
+                        Poll::Ready(Some(Ok(value))) => (
+                            MustEmitStreamItem { is_first: false },
                             Poll::Ready(Some(match serde_json::to_string(&value) {
                                 Ok(serialized_value) => {
-                                    Ok(Bytes::from(indent(serialized_value, is_first)))
+                                    Ok(Bytes::from(indent(serialized_value, *is_first)))
                                 },
                                 Err(e) => Err(SerializedStreamError::SerializationError(e)),
-                            }))
-                        },
+                            })),
+                        ),
                     },
-                    Done => Poll::Ready(None),
+                    Done => (Done, Poll::Ready(None)),
                 }
             },
         }
+    }
+}
+
+#[derive(Clone)]
+enum State<K> {
+    Init,
+    MustLoad,
+    MustEmitGlobalOpeningAndItemOpening(K),
+    MustEmitItemClosingAndItemOpening(K),
+    MustEmitGlobalOpeningAndGlobalClosing,
+    MustEmitItemClosingAndGlobalClosing,
+    MustEmitStreamItem { is_first: bool },
+    Done,
+}
+
+fn indent(serialized_value: String, is_first: bool) -> String {
+    let lines: Vec<&str> = serialized_value.split('\n').collect();
+    let mut text: String = lines.join("\n        ");
+    if is_first {
+        text.insert_str(0, "\n        ");
+    } else {
+        text.insert_str(0, ",\n        ");
+    }
+    text
+}
+
+impl<O, E, I, F, G, A, B, K, T, W, WE> Stream for JsonMapStream<O, E, I, F, G, A, B, K, T, W, WE>
+where
+    O: 'static + Send + Unpin + Serialize,
+    E: 'static + ResponseError,
+    F: 'static + Fn(sqlx::Error) -> E,
+    A: 'static + Send + IntoArguments<'static, Postgres>,
+    B: 'static + Send + Sync + Fn(<Postgres as sqlx::Database>::Row) -> Result<T, sqlx::Error>,
+    I: 'static + Iterator<Item = (K, sqlx::query::Map<'static, Postgres, B, A>)>,
+    K: 'static + Clone + Send + Unpin + Serialize,
+    T: 'static + Send + Unpin,
+    W: 'static + Unpin + Stream<Item = Result<O, WE>>,
+    WE: 'static + Sized + ResponseError,
+    G: 'static + Fn(JsonMapInnerStream<T, F>) -> W,
+{
+    type Item = Result<Bytes, SerializedStreamError<WE>>;
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        let (new_state, poll_result) = self.as_mut().new_state_poll_result(cx);
+        match new_state {
+            MustLoad => self.load_next_query_and_update_state(),
+            _ => {
+                let JsonMapStreamProject { state, .. } = self.project();
+                *state = new_state
+            },
+        }
+        poll_result
     }
 }
